07c4a595d9ace11cdc217b2183e06ba1328d1973
potatogit <potato@potatodeMacBook-Pro.local>
2015-06-22 22:55:53 +0900

    test udp

--- ChatUI.java
+++ ChatUI.java
@@ -1,134 +1,50 @@
-import java.util.*;
-import javax.swing.*;
 import java.net.*;
-import java.io.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-public class ChatUI extends JFrame {
-	public static void main(String argc[]){
-		//ChatUI c=new ChatUI();
-		ChatConnection cc=new ChatConnection("192.168.0.101");
-		cc.sendMessage("hello");
-		Receive r=new Receive(cc);
-	}
-	
-	public static InetAddress host;
-	private static JPanel contentPane;
-	private static String name;
-	public static JTextArea textArea;
-	ChatConnection chatConnection=null;
-	Receive receive=null;
-	private boolean isFree=true;
-	public ChatUI(){
-		setMenu();
-	}	
-	private void setMenu(){
-		//SwingUtilities.updateComponentTreeUI(this);
-		try {
-			host=InetAddress.getLocalHost();
-		}catch(Exception e){}
-		name="Chat       host IP: "+host.getHostAddress();
-		setTitle(name);
-		setResizable(false);//æ”¹å˜å¤§å°
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//å…³é—­æŒ‰é’®çš„è®¾å®š
-		setBounds(200,100,450,450);//xè½´yè½´çš„è·ç¦»ï¼Œé•¿å®½
-		contentPane = new JPanel() ;
-		//{
-//			//private static final long serialVersionUID = 1L;
-//
-//		};
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		//IP address field
-		final JTextField textField=new JTextField();
-		textField.setBounds(20,10,200,30);
-		getContentPane().add(textField);
-
-		// èŠå¤©ä¿¡æ¯æ˜¾ç¤ºåŒºåŸŸ
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(25, 50, 400, 250);
-		getContentPane().add(scrollPane);
-
-		textArea = new JTextArea();
-		textArea.setEditable(false);
-		textArea.setLineWrap(true);//æ¿€æ´»è‡ªåŠ¨æ¢è¡ŒåŠŸèƒ½ 
-		textArea.setWrapStyleWord(true);//æ¿€æ´»æ–­è¡Œä¸æ–­å­—åŠŸèƒ½ 
-		textArea.setFont(new Font("sdf", Font.BOLD, 13));
-		scrollPane.setViewportView(textArea);
-
-		// æ‰“å­—åŒºåŸŸ
-		JScrollPane scrollPane_1 = new JScrollPane();
-		scrollPane_1.setBounds(25, 320, 400, 70);
-		getContentPane().add(scrollPane_1);
-
-		final JTextArea textArea_1 = new JTextArea();
-		textArea_1.setLineWrap(true);//æ¿€æ´»è‡ªåŠ¨æ¢è¡ŒåŠŸèƒ½ 
-		textArea_1.setWrapStyleWord(true);//æ¿€æ´»æ–­è¡Œä¸æ–­å­—åŠŸèƒ½ 
-		scrollPane_1.setViewportView(textArea_1);
-
-		
-		JButton btnNewButton = new JButton("Send");
-		btnNewButton.setBounds(350,390,50,36);	
-		//getRootPane().setDefaultButton(btnNewButton);
-		getContentPane().add(btnNewButton);
-		
-		JButton btnNewButton1 = new JButton("Connect");
-		btnNewButton1.setBounds(235,10,80,30);
-		getContentPane().add(btnNewButton1);
-		
-		JButton btnNewButton2 = new JButton("Disconnect");
-		btnNewButton2.setBounds(320,10,100,30);
-		getContentPane().add(btnNewButton2);
-		
-		this.setVisible(true);
-		// sendæŒ‰é’®
-		btnNewButton.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-				String info = textArea_1.getText();
-				// è‡ªå·±å‘çš„å†…å®¹ä¹Ÿè¦ç°å®åœ¨è‡ªå·±çš„å±å¹•ä¸Šé¢
-				textArea.append(" æˆ‘è¯´:\r\n" + info + "\r\n");
-				try{
-					chatConnection.sendMessage(info);
-				}catch(Exception eee){
-					JOptionPane.showMessageDialog(getContentPane(), "Sending message Failed");
-				}
-				textArea_1.setText(null);
-				textArea_1.requestFocus();
-			}
-		});
-		
-		// connectæŒ‰é’®
-		btnNewButton1.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-				if(isFree&&textField.getText()!=null){
-					String ipAddr=textField.getText();
-					try{
-						 chatConnection=new ChatConnection(ipAddr);
-						// chatConnection.displayReceivedMessage();
-						 receive=new Receive(chatConnection);
-						 //System.out.println(ipAddr);
-						 isFree=false;
-						 JOptionPane.showMessageDialog(getContentPane(), "Connection Succeeded");
-					}catch(Exception ee){
-						JOptionPane.showMessageDialog(getContentPane(), "Connection Failed");
-					}
-				}
-			}
-		});
-		
-		// disconnectæŒ‰é’®
-		btnNewButton2.addActionListener(new ActionListener(){//
-			public void actionPerformed(ActionEvent e) {
-					try{
-						chatConnection.chatConnectionClose();
-						isFree=true;
-						JOptionPane.showMessageDialog(getContentPane(), "Disconnect successfully");
-					}catch(Exception ee){}
-			}
-		});
-		
-	}
-}
+    import java.util.*;
+    /**
+     * ¼òµ¥µÄUDP¿Í»§¶Ë£¬ÊµÏÖÏò·şÎñÆ÷¶Ë·¢ÉúÏµÍ³Ê±¼ä¹¦ÄÜ
+     * ¸Ã³ÌĞò·¢ËÍ3´ÎÊı¾İµ½·şÎñÆ÷¶Ë
+     */
+    public class ChatUI {
+             public static void main(String[] args) {
+             DatagramSocket ds = null; //Á¬½Ó¶ÔÏó
+             DatagramPacket sendDp; //·¢ËÍÊı¾İ°ü¶ÔÏó
+             DatagramPacket receiveDp; //½ÓÊÕÊı¾İ°ü¶ÔÏó
+             String serverHost = "127.0.0.1"; //·şÎñÆ÷IP
+             int serverPort = 10012; //·şÎñÆ÷¶Ë¿ÚºÅ
+             try{
+                    //½¨Á¢Á¬½Ó
+                    ds = new DatagramSocket();
+                    //³õÊ¼
+                    InetAddress address = InetAddress.getByName(serverHost);
+                    byte[] b = new byte[1024];
+                    receiveDp = new DatagramPacket(b,b.length);
+                    System.out.println("¿Í»§¶Ë×¼±¸Íê³É");
+                    //Ñ­»·30´Î£¬Ã¿´Î¼ä¸ô0.01Ãë
+                    for(int i = 0;i < 30;i++){
+                    	//³õÊ¼»¯·¢ËÍÊı¾İ
+                        Date d = new Date(); //µ±Ç°Ê±¼ä
+                        String content = d.toString(); //×ª»»Îª×Ö·û´®
+                        byte[] data = content.getBytes();
+                        //³õÊ¼»¯·¢ËÍ°ü¶ÔÏó
+                        sendDp = new DatagramPacket(data,data.length,address, serverPort);
+                        //·¢ËÍ
+                        ds.send(sendDp);
+                        //ÑÓ³Ù
+                        Thread.sleep(10);
+                        //½ÓÊÕ
+                        ds.receive(receiveDp);
+                        //¶ÁÈ¡·´À¡ÄÚÈİ£¬²¢Êä³ö
+                        byte[] response = receiveDp.getData();
+                        int len = receiveDp.getLength();
+                        String s = new String(response,0,len);
+                        System.out.println("·şÎñÆ÷¶Ë·´À¡Îª£º" + s);
+                   }
+       }catch(Exception e){e.printStackTrace();}
+      finally{
+    	  try{
+    		  //¹Ø±ÕÁ¬½Ó
+    		  ds.close();}
+    	  catch(Exception e){}
+    	  }
+      }
+ }
